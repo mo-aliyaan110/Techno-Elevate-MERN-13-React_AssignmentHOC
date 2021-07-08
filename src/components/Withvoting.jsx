2bbd973c64a3679cf0a4e8e84080e4610c1106e3
mo-aliyaan110 <mohammed007aliyan@gmail.com>
2021-07-08 06:42:10 +0530

    first

---
+++ src/components/Withvoting.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+function  withVoting(orgComp) { 
+    return (
+        class VotingLogic extends Component{
+            
+        }
+            
+        
+    )
+}
+export default withVoting;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import Voting from './components/Withvoting';
+function Index() {
+    return (
+        <div>
+            <WithVoting/>
+        </div>
+    )
+}
+ReactDom.render(<Index/>, document.getElementById('root'));
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
